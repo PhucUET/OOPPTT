bb9b0a16f7ce8fd1833a6a34b2f73931525300e4
24020337 <24020337@vnu.edu.vn>
2025-10-05 21:30:58 +0700

    add Collision

---
+++ src/main/java/unknown/oopptt/physic/AABB.java
@@ -0,0 +1,14 @@
+package unknown.oopptt.physic;
+
+/**
+ * thay cho rectangle với kiểu double
+ */
+public class AABB {
+    public double x,y,w,h;
+    public AABB(double x, double y, double w, double h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+}
